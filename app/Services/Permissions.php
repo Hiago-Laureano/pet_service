81e9ebbdcdcfc4ad80ea79a20302aa4ca4ae3ac9
Hiago Laureano <hiagolaureano@hotmail.com>
2024-12-23 16:32:17 -0300

    added permissions

--- app/Services/Permissions.php
+++ app/Services/Permissions.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Pet;
+use App\Models\Scheduling;
 use App\Models\User;
 
 class Permissions
@@ -20,5 +21,10 @@ class Permissions
     public static function IsSuperuserOrMyPet(User $user, int $petId): bool
     {
         return ($user->is_superuser ? true : false) or ($user->id === Pet::find($petId)->user_id ? true : false);
+    }
+
+    public static function IsSuperuserOrMyScheduling(User $user, int $schedulingId): bool
+    {
+        return ($user->is_superuser ? true : false) or ($user->id === Scheduling::find($schedulingId)->user_id ? true : false);
     }
 }
--- tests/Feature/unit/PermissionsTest.php
+++ tests/Feature/unit/PermissionsTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Pet;
+use App\Models\Scheduling;
+use App\Models\Service;
 use App\Models\User;
 use App\Services\Permissions;
 
@@ -9,6 +11,9 @@ beforeEach(function(){
     $this->superUser =User::create(["first_name" => "Super", "last_name" => "User", "phone" => 5500000000000, "email" => fake()->unique()->email, "password" => "12345", "is_superuser" => true]);
     $this->petNormalUser = Pet::create(["user_id" => $this->normalUser->id, "name" => "petNormal", "species" => "x", "breed" => "y", "weight" => 20.0, "gender" => "M", "agressive" => true]);
     $this->petSuperUser = Pet::create(["user_id" => $this->superUser->id, "name" => "petSuper", "species" => "x", "breed" => "y", "weight" => 20.0, "gender" => "M", "agressive" => true]);
+    $this->service = Service::create(["name" => "service-x", "price" => 30.99]);
+    $this->schedulingNormalUser = Scheduling::create(["user_id" => $this->normalUser->id, "pet_id" => $this->petNormalUser->id, "service_id" => $this->service->id, "date" => "2025-02-20 13:20:00", "finished" => false]);
+    $this->schedulingSuperUser = Scheduling::create(["user_id" => $this->superUser->id, "pet_id" => $this->petSuperUser->id, "service_id" => $this->service->id, "date" => "2025-02-20 13:20:00", "finished" => false]);
 });
 
 test("Function IsSuperuser return true with superuser", function(){
@@ -48,5 +53,20 @@ test("Function IsSuperuserOrMyPet return true with owner user", function(){
 
 test("Function IsSuperuserOrMyPet return false not superuser and owner", function(){
     $result = Permissions::IsSuperuserOrMyPet($this->normalUser, $this->petSuperUser->id);
+    expect($result)->toBe(false);
+});
+#----------------------------------------------------------------------
+test("Function IsSuperuserOrMyScheduling return true with superuser", function(){
+    $result = Permissions::IsSuperuserOrMyScheduling($this->superUser, $this->schedulingNormalUser->id);
+    expect($result)->toBe(true);
+});
+
+test("Function IsSuperuserOrMyScheduling return true with owner user", function(){
+    $result = Permissions::IsSuperuserOrMyScheduling($this->normalUser, $this->schedulingNormalUser->id);
+    expect($result)->toBe(true);
+});
+
+test("Function IsSuperuserOrMyScheduling return false not superuser and owner", function(){
+    $result = Permissions::IsSuperuserOrMyScheduling($this->normalUser, $this->schedulingSuperUser->id);
     expect($result)->toBe(false);
 });
