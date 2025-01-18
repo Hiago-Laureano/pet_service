df531344f1c4c23eb40be339957429a5c779db20
Hiago Laureano <hiagolaureano@hotmail.com>
2025-01-17 21:23:36 -0300

    added text for mail

---
+++ lang/en/mail.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'subject_medical_record' => 'The medical record result is available.',
+    'content_medical_record' => 'The access code for your pet medical record is:',
+
+];
---
+++ lang/pt-BR/mail.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'subject_medical_record' => 'O resultado do prontuário médico está disponível.',
+    'content_medical_record' => 'O código de acesso ao prontuário médico do seu pet é:',
+
+];
